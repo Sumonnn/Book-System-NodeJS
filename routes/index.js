5adfc5cff73fec4598dd1bb59f21e6c6f321a36a
Sumon <sumonkhan1081@gmail.com>
2023-09-25 00:58:05 +0530

    added delete feature

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,14 @@ const fs = require('fs');
 const countrylist = require('../public/javascripts/country.js');
 const languageslist = require('../public/javascripts/language.js');
 
+//utility function read and write
+function Books() {
+  return JSON.parse(fs.readFileSync("public/books.json", "utf-8"));
+}
+
+function WriteBooks(book) {
+  fs.writeFileSync("public/books.json", JSON.stringify(book));
+}
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -20,19 +28,30 @@ router.get('/register',(req,res)=>{
 
 //data save route
 router.post('/save',(req,res)=>{
-  const bookList = JSON.parse(fs.readFileSync('public/books.json','utf-8'));
+  const bookList = Books();
   bookList.push(req.body);
-  fs.writeFileSync('public/books.json',JSON.stringify(bookList));
+  WriteBooks(bookList);
   res.redirect('/show');
 })
 
 //books data show route
 router.get('/show',(req,res)=>{
-  const bookList = JSON.parse(fs.readFileSync('public/books.json','utf-8'));
-  // console.log(typeof bookList);
+  const bookList = Books();
   res.render('show.ejs',{
     bookList:bookList,
   });
 })
 
+//book data delete route
+router.get('/delete/:index',(req,res)=>{
+  const bookList = Books();
+  bookList.splice(req.params.index,1);
+  WriteBooks(bookList);  
+  res.redirect('/show');
+}) 
+
+//book data update route
+router.get('/update/:index',(req,res)=>{
+  
+})
 module.exports = router;
